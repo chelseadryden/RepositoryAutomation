f56eaf3a1837fd6ae8711bedac247971f2f72819
chelseadryden <chelseadryden@ncfe.org.uk>
2017-12-08 13:46:26 +0000

    Commit to branch one

---
+++ ChelseasAutomation/ChelseasTests/Features/Feature1.feature
@@ -0,0 +1,8 @@
+﻿Feature: Feature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@myTest
+Scenario: TestThis
+	Given I have tested this test when I'm doing something random
---
+++ ChelseasAutomation/PageObjects/Hook.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace PageObjects
+{
+    [Binding]
+    [TestClass]
+    public sealed class Hook
+    {
+        private static SharedMethods sharedMethods;
+
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            sharedMethods = new SharedMethods();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            sharedMethods.CloseDriver();
+        }
+    }
+}
